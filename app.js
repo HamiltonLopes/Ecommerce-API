f363945f01f5580691fd4190380d233970bd4682
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-06-17 17:11:45 -0300

    adding pagar.me

--- app.js
+++ app.js
@@ -26,12 +26,11 @@ app.use('/users', usersRouter);
 app.use(`/${process.env.API_NAME}/${process.env.API_VERSION}/compras`, comprasRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/compras.js
+++ routes/compras.js
@@ -1,35 +1,74 @@
 var express = require('express');
 var router = express.Router();
-var cielo = require('../lib/cielo.js');
+var payment = require('../lib/payment.js');
 
 /* POST criar compras. */
 router.post('/', async function (req, res, next) {
-  const result = await cielo.compra(req.body);
-  const captureResult = await cielo.captura(result.Payment.PaymentId);
-  return captureResult.Status == 2 ? res.status(200).json({ message: "Compra efetuada com sucesso.",
-    paymentId: result.Payment.PaymentId }) : res.status(402).json({ error: "Compra não processada, problema no cartão." });
+
+  let result;
+  let captureResult;
+  let paymentId;
+  let error;
+
+  try {
+    result = await payment.compra(req.body, "Cielo");
+    paymentId = result.data.Payment.PaymentId;
+    captureResult = await payment.captura(paymentId, result.company);
+  } catch (error) {
+    console.error(error);
+    error = true;
+  }
+
+  if(error || captureResult.Status != 2){
+    console.log("Erro de comunicação com Cielo, transferindo transação para Pagarme.");
+    try {
+      result = await payment.compra(req.body, "Pagarme");
+      paymentId = result.data.data.id;
+      captureResult = await payment.captura(paymentId, result.company);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Erro interno no servidor!" });
+    }
+  }
+
+  return (captureResult.Status == 2 || captureResult.status == 200) ?
+    res.status(200).json({
+      message: "Compra efetuada com sucesso.",
+      company: result.company,
+      paymentId: paymentId
+    }) :
+    res.status(402).json({
+      error: "Compra não processada, problema no cartão.",
+      company: result.company
+    });
+
 });
 
 /* GET status compras. */
 router.get('/:compra_id/status', async function (req, res, next) {
-  const {compra_id} = req.params;
-  const result = await cielo.consulta(compra_id);
-  let resJson = {message : ""};
-  switch(result.Payment.Status){
-    case 1:
-      resJson.message = "Pagamento Autorizado.";
-      break;
-    case 2:
-      resJson.message = "Pagamento Confirmado.";
-      break;
-    case 12:
-      resJson.message = "Pagamento Pendente.";
-      break;
-    default:
-      resJson.message = "Erro no pagamento.";
-      break;
+  const { compra_id } = req.params;
+  try{
+    const result = await payment.consulta(compra_id);
+  }catch(error){
+    console.error(error.response.data);
   }
-  return res.status(200).json(resJson);
+  res.send("ok");
+  // let resJson = { message: "" };
+  // switch (result.Payment.Status) {
+  //   case 1:
+  //     resJson.message = "Pagamento Autorizado.";
+  //     break;
+  //   case 2:
+  //     resJson.message = "Pagamento Confirmado.";
+  //     break;
+  //   case 12:
+  //     resJson.message = "Pagamento Pendente.";
+  //     break;
+  //   default:
+  //     resJson.message = "Erro no pagamento.";
+  //     break;
+  // }
+  // return res.status(200).json(resJson);
 });
 
 module.exports = router;
